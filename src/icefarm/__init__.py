d7af4163008f1b3be06f5dc911e9ec16f5315445
heiljj <heiljj@rose-hulman.edu>
2026-02-07 23:55:18 -0600

    fixed packaging part 2

--- src/icefarm/__init__.py
+++ src/icefarm/__init__.py
@@ -1,3 +1,13 @@
-from icefarm import control
-from icefarm import worker
+# TODO need to reevaluate pyproject scripts,
+
+try:
+    from icefarm.control import app
+except ImportError:
+    app = None
+
+try:
+    from icefarm import worker
+except ImportError:
+    worker = None
+
 from icefarm import client
